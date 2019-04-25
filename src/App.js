2574ea1139ff95f29df996c493bf878b60f30575
dashu19 <46594899+dashu19@users.noreply.github.com>
2019-04-25 17:51:38 -0400

    need to implement way of checking if all drinks in list direct to working links

--- src/App.js
+++ src/App.js
@@ -78,7 +78,7 @@ class Bartender extends React.Component {
   constructor(props){
     super(props);
     this.state = {
-      initial: false,
+      initial: true,
       data: 'initial',
       drinklinks: []
     };
@@ -96,8 +96,19 @@ class Bartender extends React.Component {
       });
     }
 
-    console.log(drinklist);
+    // console.log(drinklist);
     this.setState({data: drinklist});
+    this.setState({initial: false})
+  }
+
+  getDrinkInfo = async () => {
+    const url = 'https://en.wikipedia.org/w/api.php?action=parse&origin=*&prop=wikitext&section=0&format=json&page=';
+    const url1 = url + this.state.data[53];
+    const response = await fetch(url1);
+    const json = await response.json();
+    let parsed = json.parse.wikitext['*'];
+    console.log(typeof parsed);
+    console.log(parsed);
   }
 
   fetchtest3 = async () => {
@@ -107,22 +118,31 @@ class Bartender extends React.Component {
     this.setState({data: processed});
   }
 
+  dummylist = () => {
+    let ret = []
+    this.state.data.forEach(function(item, i){
+      ret.push(<li key={i}>{item}</li>);
+    });
+    return ret;
+
+  }
+
 
   componentDidMount = async () => {
     //await this.fetchtest3();
     await this.getDrinkList();
+    await this.getDrinkInfo();
     console.log('endofdidmount');
   }
 
   render = () => {
-    let string = this.state.data;
     if (this.state.initial === true){
       return(
-        <div className="Bartender">{string}</div>
+        <div className="Bartender"></div>
       );
     } else {
       return(
-        <div className="Bartender">{string}</div>
+        <div className="Bartender"><ul>{this.dummylist()}</ul></div>
       );
     }
   }
