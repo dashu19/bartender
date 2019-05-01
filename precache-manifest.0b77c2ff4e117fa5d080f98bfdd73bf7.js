c42834ce4b79fd4b259cdb2d935e85545f753722
dashu <35853700+dshuang1@users.noreply.github.com>
2019-05-01 02:00:56 -0400

    Updates

---
+++ precache-manifest.0b77c2ff4e117fa5d080f98bfdd73bf7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d3b9c477c325f64180ec545ebd0ed06f",
+    "url": "/bartender/index.html"
+  },
+  {
+    "revision": "7a506f3ab82f421609c6",
+    "url": "/bartender/static/css/main.dfd89ae6.chunk.css"
+  },
+  {
+    "revision": "954f13652495895a7fb5",
+    "url": "/bartender/static/js/2.c3c80b0d.chunk.js"
+  },
+  {
+    "revision": "7a506f3ab82f421609c6",
+    "url": "/bartender/static/js/main.9f4995fd.chunk.js"
+  },
+  {
+    "revision": "57c9d559ca1429949dfb",
+    "url": "/bartender/static/js/runtime~main.a6274a12.js"
+  }
+]);
